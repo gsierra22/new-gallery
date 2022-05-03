677f0c8e7351f9162e3a0a1d237d545ddbb13b3c
gsierra22 <giancarlosierra@Giancarlos-MacBook-Pro.local>
2022-05-03 00:14:45 -0500

    get test working

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 //requires
 const express = require ('express')
 const app = express();
+const gallery = require ('./routes/gallery.route')
 
 //uses
+app.use('/gallery',gallery)
 
 //globals
 const port = 5000
 
-app.list(port, ()=>{
+app.listen(port, ()=>{
     console.log('server:', port)
 })
