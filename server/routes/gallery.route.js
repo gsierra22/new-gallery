31d412b5db5f342174b512e0f106e556b3894bdb
gsierra22 <giancarlosierra2011@hotmail.com>
2022-05-08 22:39:06 -0500

    git route working

--- server/routes/gallery.route.js
+++ server/routes/gallery.route.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
+const pool = require('../modules/pool')
 
 router.get('/', (req,res)=>{
     console.log('/gallery GET')
-    res.send('email config')
+    const queryString = `SELECT * FROM "gallery_items"`;
+    pool.query(queryString).then((results)=>{
+        res.send(results.rows);
+    }).catch((err)=>{
+        console.log(err);
+        res.sendStatus(500);
+    })
 })
 
 module.exports = router
