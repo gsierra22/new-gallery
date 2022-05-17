e8dd56ddf75b249170e6125fdea0b0f1e2bed4d5
gsierra22 <giancarlosierra2011@hotmail.com>
2022-05-16 21:39:52 -0500

    gallery list made

---
+++ src/GalleryList/GalleryList.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+function _template(props){
+
+    return(
+        <div>
+            <h1>_template</h1>
+            <h3>{JSON.stringify(props)}</h3>
+        </div>
+    )
+}
+
+export default _template
